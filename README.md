506559801266471acf2e7c169cb01a5b9ee84852
CoderJ <550106786@qq.com>
2020-07-07 15:46:51 +0800

    initial commit.
    Signed-off-by: CoderJ <550106786@qq.com>

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # rematch-immer-combine-persist
-rematch plugin combine immer and persist
+
+## CHANGELOG
+
+- [CHANGELOG.md](https://github.com/Coder-JJ/rematch-immer-combine-persist/blob/master/CHANGELOG.md)
+
+## Docs
+
+- [immer](https://github.com/immerjs/immer)
+
+- [redux-persist](https://github.com/rt2zz/redux-persist)
+
+- [@rematch/immer](https://rematch.github.io/rematch/#/plugins/immer?id=usage)
+
+- [@rematch/persist](https://rematch.github.io/rematch/#/plugins/persist?id=setup)
+
+## Install
+
+```cmd
+npm i rematch-immer-combine-persist -S
+```
+
+## Usage
+
+```js
+// store.js
+import { init } from '@rematch/core'
+import immerWithPersist from 'rematch-immer-combine-persist'
+import * as models from './models'
+
+const store = init({
+  models,
+  plugins: [
+    immerWithPersist({
+      // see @rematch/persist docs...
+    })
+  ]
+})
+
+export default store
+```
+
+```jsx
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { getPersistor } from '.rematch-immer-combine-persist'
+import store from './store'
+import App from './App'
+
+const persistor = getPersistor()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+)
+```
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { ReducersMapObject } from 'redux'
+import { Plugin, Action, Models } from '@rematch/core'
+import produce from 'immer'
+import { persistCombineReducers, persistStore, Persistor, PersistorOptions, PersistConfig } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+let persistor: Persistor
+
+export const getPersistor = (): Persistor => persistor
+
+const plugin = (
+  persistConfig: Omit<PersistConfig<any, any, any, any>, 'key' | 'storage'> = {},
+  persistStoreConfig?: PersistorOptions | null,
+  callback?: () => any
+): Plugin => {
+  const persistMergedConfig: PersistConfig<any, any, any, any> = {
+    key: 'root',
+    storage,
+    ...persistConfig
+  }
+  return {
+    config: {
+      redux: {
+        combineReducers (reducers) {
+          const reducersWithImmer: ReducersMapObject<any, Action<any>> = {}
+          for (const key of Object.keys(reducers)) {
+            const reducer = reducers[key]
+            reducersWithImmer[key] = (state, payload) => {
+              if (typeof state === 'object') {
+                return produce(state, (draft: Models) => {
+                  const next = reducer(draft, payload)
+                  if (typeof next === 'object') {
+                    return next
+                  }
+                })
+              }
+              return reducer(state, payload)
+            }
+          }
+          return persistCombineReducers(persistMergedConfig, reducersWithImmer)
+        }
+      }
+    },
+    onStoreCreated (store) {
+      persistor = persistStore(store, persistStoreConfig, callback)
+    }
+  }
+}
+
+export default plugin
